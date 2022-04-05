ad1935d28418ca2f9ababe34278ab09fd99153ff
montoyamoraga <montoyamoraga@gmail.com>
2022-04-05 09:37:17 -0400

    agregar comentarios

--- clases/clase-04/ejemplo_dado/ejemplo_dado.ino
+++ clases/clase-04/ejemplo_dado/ejemplo_dado.ino
@@ -1,12 +1,34 @@
+// ejemplo_dado
+// v0.0.1
+// abril 2022
+// para Arduino Uno
+// programado en Arduino IDE 1.8.19
+
+// valores minimos y maximos de dado
 int minDado = 1;
 int maxDado = 6;
 
+// setup() ocurre una vez, al principio
 void setup() {
+
+  // iniciar comunicacion serial
   Serial.begin(9600);
+
 }
 
+// loop() ocurre en bucle, despues de setup
 void loop() {
+
+  // random() arroja valor aleatorio en [min, max[
+  int resultado = random(minDado, maxDado + 1);
+
+  // imprimir texto
   Serial.print("dado: ");
-  Serial.println(random(minDado, maxDado + 1));
+
+  // imprimir resultado y nueva linea
+  Serial.println(resultado);
+
+  // esperar 1 segundo
   delay(1000);
+  
 }
