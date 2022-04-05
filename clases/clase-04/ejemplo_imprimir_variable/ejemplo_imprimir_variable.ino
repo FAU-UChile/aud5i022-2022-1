ec72ded8694adfed76d03414ee49b453cacd3b11
montoyamoraga <montoyamoraga@gmail.com>
2022-04-05 09:46:50 -0400

    agregar comentarios

--- clases/clase-04/ejemplo_imprimir_variable/ejemplo_imprimir_variable.ino
+++ clases/clase-04/ejemplo_imprimir_variable/ejemplo_imprimir_variable.ino
@@ -1,10 +1,29 @@
+// ejemplo_imprimir_variable
+// por montoyamoraga
+// v0.0.1 abril 2022
+// hecho con Arduino Uno y IDE 1.8.19
+
+// declarar variable x
+// y asignar valor inicial 1
 int x = 1;
 
+// setup() ocurre una vez al principio
 void setup() {
+
+  // abrir comunicacion serial
   Serial.begin(9600);
+
 }
 
+
+// loop() ocurre despues de setup(), en bucle
 void loop() {
-  x = x + 1;
+
+  // imprimir valor actual de variable x
+  // imprimir nueva linea
   Serial.println(x);
+  
+  // aumentar valor de x
+  x = x + 1;
+
 }
--- clases/clase-04/ejemplo_led_interno/ejemplo_led_interno.ino
+++ clases/clase-04/ejemplo_led_interno/ejemplo_led_interno.ino
@@ -1,12 +1,32 @@
+// ejemplo_led_interno
+// por montoyamoraga
+// v0.0.1 abril 2022
+// hecho con Arduino Uno y IDE 1.8.19
+
+// variable para pin con LED interno
 int pinLED = 13;
 
+// setup() ocurre una vez, al principio
 void setup() {
+  
+  // hacer el pin digital sea salida (OUTPUT)
   pinMode(pinLED, OUTPUT);
+  
 }
 
+// loop() ocurre despues de setup(), en bucle
 void loop() {
+  
+  // escribir en el pin 1 o voltaje lógico alto (HIGH)
   digitalWrite(pinLED, HIGH);
+
+  // esperar 1 segundo
   delay(1000);
+  
+  // escribir en el pin 0 o voltaje lógico bajo (LOW)
   digitalWrite(pinLED, LOW);
+
+  // esperar 1 segundo
   delay(1000);
+  
 }
