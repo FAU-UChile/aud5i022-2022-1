5451eb905ba3748ff570416a7d35c0abc7092321
montoyamoraga <montoyamoraga@gmail.com>
2022-04-12 09:39:46 -0400

    imprimir valor

--- clases/clase-05/ej_02_potenciometro/ej_02_potenciometro.ino
+++ clases/clase-05/ej_02_potenciometro/ej_02_potenciometro.ino
@@ -1,9 +1,17 @@
 // ej_02_potenciometro
 
 // potenciometro
-// pin central conectado a entrada analoga 0
-// pin arriba conectado a 5V
-// pin abajo conectado a tierra
+// orientacion texto hacia derecha
+// orientacion perilla hacia arriba
+// conectar en columna e con pins:
+// pin1 conectado a e5
+// pin2 conectado a e6
+// pin3 conectado a e7 
+
+// arduino uno
+// 5V conectado a pin1 potenciometro (fila 5)
+// A0 conectado a pin2 potenciometro (fila 6)
+// GND conectado a pin3 potenciometro (fila 7)
 
 int pinEntrada = A0;
 
@@ -13,6 +21,7 @@ int valorSensor = 0;
 
 void setup() {
   pinMode(pinSalida, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -20,16 +29,19 @@ void loop() {
   // actualizar valor sensor
   valorSensor = analogRead(pinEntrada);
 
+  // imprimir valor sensor
+  Serial.println(valorSensor);
+
   // prender LED
-  digitalWrite(pinSalida, HIGH);
+//  digitalWrite(pinSalida, HIGH);
 
   // detener el programa
-  delay(valorSensor);
+//  delay(valorSensor);
 
   // apagar LED
-  digitalWrite(pinSalida, LOW);
+//  digitalWrite(pinSalida, LOW);
 
   // detener el programa
-  delay(valorSensor);
+//  delay(valorSensor);
   
 }
